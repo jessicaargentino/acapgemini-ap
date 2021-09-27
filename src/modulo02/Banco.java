2770fe54e9afa389c0322885b185435854ead1a3
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-09-27 11:34:35 -0300

    A12 - Cabeçalho feito

---
+++ src/modulo02/Banco.java
@@ -0,0 +1,13 @@
+package modulo02;
+
+import java.util.Scanner;
+
+public class Banco {
+    public static void main(String[] args) {
+        cabecalho();
+    }
+
+    static void cabecalho(){
+        System.out.println("****** BRADESCO FINANCIAMENTOS ******");
+    }
+}
