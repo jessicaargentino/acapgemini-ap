977b3dd19a1e8975bbb3dff16c75867e4370c82d
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-09-29 14:54:44 -0300

    A14 - Adição de comentários, código finalizado

--- src/modulo02/aula14/Pessoa.java
+++ src/modulo02/aula14/Pessoa.java
@@ -1,14 +1,17 @@
 package modulo02.aula14;
 
 public class Pessoa {
+    // DEFINIÇÃO DOS ATRIBUTOS DA CLASSE
     String nome;
     String sobrenome;
     int idade;
 
     public static void main(String[] args) {
+        // CRIAÇÃO DE DOIS OBJETOS ATRAVÉS DA INSTÂNCIA DA CLASSE PESSOA
         Pessoa pessoa1 = new Pessoa();
         Pessoa pessoa2 = new Pessoa();
 
+        // PASSAGEM DE VALORES PARA OS ATRIBUTOS DA CLASSE
         pessoa1.nome = "Laura";
         pessoa1.sobrenome = "Palmer";
         pessoa1.idade = 17;
@@ -17,6 +20,7 @@ public class Pessoa {
         pessoa2.sobrenome = "Skywalker";
         pessoa2.idade = 53;
 
+        // IMPRESSÃO DOS VALORES PASSADOS PARA OS ATRIBUTOS
         System.out.println("***** PESSOAS *****");
         System.out.printf("Nome: %s %s\n", pessoa1.nome, pessoa1.sobrenome);
         System.out.printf("Idade: %d\n", pessoa1.idade);
