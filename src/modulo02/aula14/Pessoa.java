3708dcc2417fac34ec29a50826dd1c875901523e
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-09-29 14:44:30 -0300

    A14 - Passando valores para os atributos da classe

--- src/modulo02/aula14/Pessoa.java
+++ src/modulo02/aula14/Pessoa.java
@@ -3,9 +3,12 @@ package modulo02.aula14;
 public class Pessoa {
     String nome;
     String sobrenome;
-    String idade;
+    int idade;
 
     public static void main(String[] args) {
         Pessoa pessoa = new Pessoa();
+        pessoa.nome = "Jéssica";
+        pessoa.sobrenome = "Argentino";
+        pessoa.idade = 22;
     }
 }
