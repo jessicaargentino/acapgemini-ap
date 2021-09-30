df9d9da4ca17a2897453ced53b2ab337fe316eb8
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-09-30 11:51:34 -0300

    A15 - Método de calcular taxa transferência

--- src/modulo02/aula15/CalculadoraTaxas.java
+++ src/modulo02/aula15/CalculadoraTaxas.java
@@ -1,6 +1,13 @@
 package modulo02.aula15;
 
 public class CalculadoraTaxas {
+    // DEFINIÇÃO DOS ATRIBUTOS DA CLASSE
     private double taxaTransferencia;
     private double taxaSaque;
+
+    // MÉTODO QUE RECEBE O VALOR A SER TRANSFERIDO E RETORNA O VALOR COM A TAXA DE
+    // 0.001% EMBUTIDO
+    public double calculaTaxaTransferencia(double valor) {
+        return taxaTransferencia += (taxaTransferencia * 0.001);
+    }
 }
