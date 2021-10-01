3978ae968c09b12658569dc8884202ff1004eaa7
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-10-01 15:04:02 -0300

    A16 - Impressão do valor dos impostos

--- src/modulo02/aula16/Main.java
+++ src/modulo02/aula16/Main.java
@@ -12,6 +12,10 @@ public class Main {
         double iss = imposto.calculaISS(500.00);
         double iof = imposto.calculaIOF(987.67);
         double ir = imposto.calculaIR(34.90);
+
+        /* IMPRESSÃO DOS IMPOSTOS */
+        System.out.println("***** IMPOSTOS *****");
+        System.out.printf("ISS: R$%.2f\nIOF: R$%.2f\nIR: R$%.2f", iss, iof, ir);
     }
 
 }
