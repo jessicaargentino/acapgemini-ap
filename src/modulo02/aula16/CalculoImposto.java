51e55e3e6a99274aee38c30fee01515c442aa520
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-10-01 14:48:02 -0300

    A16 - Criação do método calculaISS

--- src/modulo02/aula16/CalculoImposto.java
+++ src/modulo02/aula16/CalculoImposto.java
@@ -2,7 +2,13 @@ package modulo02.aula16;
 
 public class CalculoImposto {
     // DEFINIÇÃO DOS ATRIBUTOS DA CLASSE
-    private double iss = 7.9;
-    private double iof = 3.4;
-    private double ir = 22.5;
+    private double iss = 0.079; // 7.9%
+    private double iof = 0.034; // 3.4%
+    private double ir = 0.225; // 22.5%;
+
+    // MÉTODO QUE RECEBE UM VALOR POR PARÂMETRO E CALCULA O IMPOSTO ISS SOBRE O
+    // MESMO
+    public double calculaISS(double valor) {
+        return (valor * iss);
+    }
 }
