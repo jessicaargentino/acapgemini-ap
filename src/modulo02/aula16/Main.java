eabce2cff83837c82bf20330473a318330c75ffa
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-10-01 15:03:12 -0300

    A16 - Variáveis para armazenar retorno das funções

--- src/modulo02/aula16/Main.java
+++ src/modulo02/aula16/Main.java
@@ -4,6 +4,14 @@ public class Main {
     public static void main(String[] args) {
         /* CRIAÇÃO DE UM OBJETO ATRAVÉS DA INSTÂNCIA DA CLASSE CALCULOIMPOSTO */
         CalculoImposto imposto = new CalculoImposto();
+
+        /*
+         * CRIAÇÃO DE TRÊS VARIÁVEIS PARA ARMAZENAR OS VALORES RETORNADOS PELOS MÉTODOS
+         * QUE ESTÃO SENDO CHAMADOS
+         */
+        double iss = imposto.calculaISS(500.00);
+        double iof = imposto.calculaIOF(987.67);
+        double ir = imposto.calculaIR(34.90);
     }
 
 }
