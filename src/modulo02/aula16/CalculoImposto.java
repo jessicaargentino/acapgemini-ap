4ebf02806a122d521268e459d7cda05b8cd0c2d1
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-10-01 14:49:48 -0300

    A16 - Criação do método calculaIOF

--- src/modulo02/aula16/CalculoImposto.java
+++ src/modulo02/aula16/CalculoImposto.java
@@ -1,14 +1,24 @@
 package modulo02.aula16;
 
 public class CalculoImposto {
-    // DEFINIÇÃO DOS ATRIBUTOS DA CLASSE
+    /* DEFINIÇÃO DOS ATRIBUTOS DA CLASSE */
     private double iss = 0.079; // 7.9%
     private double iof = 0.034; // 3.4%
     private double ir = 0.225; // 22.5%;
 
-    // MÉTODO QUE RECEBE UM VALOR POR PARÂMETRO E CALCULA O IMPOSTO ISS SOBRE O
-    // MESMO
+    /*
+     * MÉTODO QUE RECEBE UM VALOR POR PARÂMETRO E CALCULA O IMPOSTO ISS SOBRE O
+     * MESMO
+     */
     public double calculaISS(double valor) {
         return (valor * iss);
+    }
+
+    /*
+     * MÉTODO QUE RECEBE UM VALOR POR PARÂMETRO E CALCULA O IMPOSTO IOF SOBRE O
+     * MESMO
+     */
+    public double calculaIOF(double valor) {
+        return (valor * iof);
     }
 }
