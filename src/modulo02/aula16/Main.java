0155b13ad84927d614e245336811bc2e829fc107
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-10-01 14:56:47 -0300

    A16 - Instanciação da classe CalculoImposto

--- src/modulo02/aula16/Main.java
+++ src/modulo02/aula16/Main.java
@@ -2,7 +2,8 @@ package modulo02.aula16;
 
 public class Main {
     public static void main(String[] args) {
-        
+        /* CRIAÇÃO DE UM OBJETO ATRAVÉS DA INSTÂNCIA DA CLASSE CALCULOIMPOSTO */
+        CalculoImposto imposto = new CalculoImposto();
     }
-    
+
 }
