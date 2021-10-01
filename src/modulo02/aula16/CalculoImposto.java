3f4fda096acb76775fd142b9672f86ac04ab0e5c
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-10-01 14:42:40 -0300

    A16 - Definição dos atributos da classe

--- src/modulo02/aula16/CalculoImposto.java
+++ src/modulo02/aula16/CalculoImposto.java
@@ -1,5 +1,8 @@
 package modulo02.aula16;
 
 public class CalculoImposto {
-    
+    // DEFINIÇÃO DOS ATRIBUTOS DA CLASSE
+    private double iss = 7.9;
+    private double iof = 3.4;
+    private double ir = 22.5;
 }
