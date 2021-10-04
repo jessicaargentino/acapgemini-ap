37862be836f020407ef61215f7fdff06e4804f76
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-10-04 17:46:29 -0300

    A17 - Atribuição de valor para cada objeto pessoa

--- src/modulo02/aula17/CadastroPessoa.java
+++ src/modulo02/aula17/CadastroPessoa.java
@@ -12,6 +12,41 @@ public class CadastroPessoa {
         PessoaFisica pessoa4 = new PessoaFisica();
         PessoaFisica pessoa5 = new PessoaFisica();
 
+        /* ATRIBUIÇÃO DE VALORES PARA CADA PESSOA UTILIZANDO OS SETTERS */
+        pessoa1.setCodigo(12345);
+        pessoa1.setNome("Laura");
+        pessoa1.setSobrenome("Palmer");
+        pessoa1.setIdade(17);
+        pessoa1.setRg("00.000.000");
+        pessoa1.setCpf("000.000.000-00");
+
+        pessoa2.setCodigo(93473);
+        pessoa2.setNome("Luke");
+        pessoa2.setSobrenome("Skywalker");
+        pessoa2.setIdade(53);
+        pessoa2.setRg("11.111.111");
+        pessoa2.setCpf("111.111.111-11");
+
+        pessoa3.setCodigo(82947);
+        pessoa3.setNome("Chihiro");
+        pessoa3.setSobrenome("Ogino");
+        pessoa3.setIdade(10);
+        pessoa3.setRg("22.222.222");
+        pessoa3.setCpf("222.222.222-22");
+
+        pessoa4.setCodigo(90983);
+        pessoa4.setNome("Wirt");
+        pessoa4.setSobrenome("Jefferson");
+        pessoa4.setIdade(17);
+        pessoa4.setRg("33.333.333");
+        pessoa4.setCpf("333.333.333-33");
+
+        pessoa5.setCodigo(28374);
+        pessoa5.setNome("Pure");
+        pessoa5.setSobrenome("Vessel");
+        pessoa5.setIdade(24);
+        pessoa5.setRg("44.444.444");
+        pessoa5.setCpf("444.444.444-44");
     }
 
 }
