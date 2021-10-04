3080cbe13278d1427b043168a78a4db5e614fd2e
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-10-04 13:48:47 -0300

    A17 - Instanciação da classe

--- src/modulo02/aula17/TelaPrincipalBanco.java
+++ src/modulo02/aula17/TelaPrincipalBanco.java
@@ -2,6 +2,7 @@ package modulo02.aula17;
 
 public class TelaPrincipalBanco {
     public static void main(String[] args) {
-        
+        /* CRIAÇÃO DE UM OBJETO ATRAVÉS DA INSTÂNCIA DA CLASSE CONTACORRENTE */
+        ContaCorrente conta = new ContaCorrente();
     }
 }
