18b8a94f7a1369c94819bf832d236c08652aa865
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-10-04 17:30:47 -0300

    A17 - Criação do array de objetos PessoaFisica

--- src/modulo02/aula17/CadastroPessoa.java
+++ src/modulo02/aula17/CadastroPessoa.java
@@ -2,7 +2,8 @@ package modulo02.aula17;
 
 public class CadastroPessoa {
     public static void main(String[] args) {
-        
+        /* CRIANDO UM ARRAY DE OBJETOS PESSOAFISICA */
+        PessoaFisica[] pessoa = new PessoaFisica[5];
     }
-    
+
 }
