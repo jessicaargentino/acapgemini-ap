b635126acd38aa31677bd74ffe09805f1deb3fb0
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-10-04 13:34:22 -0300

    A17 - Declaração dos getters e setters

--- src/modulo02/aula17/ContaCorrente.java
+++ src/modulo02/aula17/ContaCorrente.java
@@ -6,4 +6,38 @@ public class ContaCorrente {
     private int numero;
     private int codigoCliente;
     private double saldo;
+
+    /* SETTERS E GETTERS */
+
+    public void setAgencia(int agencia) {
+        this.agencia = agencia;
+    }
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setCodigoCliente(int codigoCliente) {
+        this.codigoCliente = codigoCliente;
+    }
+
+    public int getCodigoCliente() {
+        return codigoCliente;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
 }
