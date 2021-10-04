c8e9af6394ee8b8c9132cc9ce5affaca3f5ba406
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-10-04 13:20:18 -0300

    A17 - Declaração dos atributos

--- src/modulo02/aula17/Produto.java
+++ src/modulo02/aula17/Produto.java
@@ -1,5 +1,9 @@
 package modulo02.aula17;
 
 public class Produto {
-    
+    /* DECLARAÇÃO DOS ATRIBUTOS DA CLASSE */
+    private int codigo;
+    private String nome;
+    private String descricao;
+    private double valor;
 }
