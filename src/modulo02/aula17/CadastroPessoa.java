3997760c1871e6723e187910b3bd592bb19c28e6
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-10-04 17:33:24 -0300

    A17 - Criação de 5 objetos da classe PessoaFisica

--- src/modulo02/aula17/CadastroPessoa.java
+++ src/modulo02/aula17/CadastroPessoa.java
@@ -4,6 +4,14 @@ public class CadastroPessoa {
     public static void main(String[] args) {
         /* CRIANDO UM ARRAY DE OBJETOS PESSOAFISICA */
         PessoaFisica[] pessoa = new PessoaFisica[5];
+
+        /* CRIANDO CINCO INSTÂNCIAS DA CLASSE PESSOAFISICA (OBJETO) */
+        PessoaFisica pessoa1 = new PessoaFisica();
+        PessoaFisica pessoa2 = new PessoaFisica();
+        PessoaFisica pessoa3 = new PessoaFisica();
+        PessoaFisica pessoa4 = new PessoaFisica();
+        PessoaFisica pessoa5 = new PessoaFisica();
+
     }
 
 }
