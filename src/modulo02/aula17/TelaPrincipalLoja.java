37ec85cf12ca6a507f95b4d80bdafd5557888de0
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-10-04 13:37:49 -0300

    A17 - Instanciação da classe produto

--- src/modulo02/aula17/TelaPrincipalLoja.java
+++ src/modulo02/aula17/TelaPrincipalLoja.java
@@ -1,5 +1,8 @@
 package modulo02.aula17;
 
 public class TelaPrincipalLoja {
-    
+    public static void main(String[] args) {
+        /* CRIAÇÃO DE UM OBJETO ATRAVÉS DA INSTÂNCIA DA CLASSE PRODUTO */
+        Produto produto = new Produto();
+    }
 }
