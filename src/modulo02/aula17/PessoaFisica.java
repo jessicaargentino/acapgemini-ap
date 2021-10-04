e7963ca25bca72830dab3851cebb9289863b49b3
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-10-04 17:27:39 -0300

    A17 - Declaração dos getters e setters

--- src/modulo02/aula17/PessoaFisica.java
+++ src/modulo02/aula17/PessoaFisica.java
@@ -8,4 +8,54 @@ public class PessoaFisica {
     private short idade;
     private String rg;
     private String cpf;
+
+    /* GETTERS E SETTERS */
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setIdade(short idade) {
+        this.idade = idade;
+    }
+
+    public short getIdade() {
+        return idade;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
 }
