edf410b9a53a02502e16fb2054e4957bd9c0a01a
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-10-04 13:26:15 -0300

    A17 - Declaração dos getters e setters

--- src/modulo02/aula17/Produto.java
+++ src/modulo02/aula17/Produto.java
@@ -6,4 +6,38 @@ public class Produto {
     private String nome;
     private String descricao;
     private double valor;
+
+    /* SETTERS E GETTERS */
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public double getValor() {
+        return valor;
+    }
 }
