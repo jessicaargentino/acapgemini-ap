062f7f1564d31d7c1df06ba2e2bdf191d076af8c
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-10-04 13:46:16 -0300

    A17 - Impressão dos valores

--- src/modulo02/aula17/TelaPrincipalLoja.java
+++ src/modulo02/aula17/TelaPrincipalLoja.java
@@ -10,12 +10,20 @@ public class TelaPrincipalLoja {
         produto.setNome("Kalimba");
         produto.setDescricao("Instrumento musical");
         produto.setValor(80.00);
-    
-        /* ACESSO DOS VALORES UTILIZANDO OS GETTERS E ARMAZENAMENTO DOS MESMOS NAS VARIÁVEIS */
+
+        /*
+         * ACESSO DOS VALORES UTILIZANDO OS GETTERS E ARMAZENAMENTO DOS MESMOS NAS
+         * VARIÁVEIS
+         */
         int codigo = produto.getCodigo();
         String nome = produto.getNome();
         String descricao = produto.getDescricao();
         double valor = produto.getValor();
 
+        /* IMPRESSÃO DOS VALORES */
+        System.out.printf("Código produto: %d\n", codigo);
+        System.out.printf("Nome: %s\n", nome);
+        System.out.printf("Descrição: %s\n", descricao);
+        System.out.printf("Valor: R$%.2f\n", valor);
     }
 }
