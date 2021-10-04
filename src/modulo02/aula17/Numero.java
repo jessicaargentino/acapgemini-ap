2461a8c59e467f2ec9e5c2641e4b799c4e1738e4
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-10-04 17:18:40 -0300

    A17 - Declaração do array de inteiros

--- src/modulo02/aula17/Numero.java
+++ src/modulo02/aula17/Numero.java
@@ -2,6 +2,7 @@ package modulo02.aula17;
 
 public class Numero {
     public static void main(String[] args) {
-
+        /* DECLARAÇÃO DO ARRAY DE INTEIROS COM 10 POSIÇÕES */
+        int numeros[] = new int[10];
     }
 }
