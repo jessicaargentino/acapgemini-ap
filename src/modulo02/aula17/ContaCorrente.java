4e6dfeca0325ffc1605ef448af722a6e3a65701a
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-10-04 13:30:28 -0300

    A17 - Declaração dos atributos

--- src/modulo02/aula17/ContaCorrente.java
+++ src/modulo02/aula17/ContaCorrente.java
@@ -1,5 +1,9 @@
 package modulo02.aula17;
 
 public class ContaCorrente {
-    
+    /* DECLARAÇÃO DOS ATRIBUTOS DA CLASSE */
+    private int agencia;
+    private int numero;
+    private int codigoCliente;
+    private double saldo;
 }
