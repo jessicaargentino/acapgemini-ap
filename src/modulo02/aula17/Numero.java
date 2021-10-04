9ab9f6cd7221ba1f4d5041ed220f8aa636ab4657
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-10-04 17:20:40 -0300

    A17 - Adição de valores para cada posição

--- src/modulo02/aula17/Numero.java
+++ src/modulo02/aula17/Numero.java
@@ -3,6 +3,19 @@ package modulo02.aula17;
 public class Numero {
     public static void main(String[] args) {
         /* DECLARAÇÃO DO ARRAY DE INTEIROS COM 10 POSIÇÕES */
-        int numeros[] = new int[10];
+        int numero[] = new int[10];
+
+        /* ATRIBUIÇÃO DE VALORES CONFORME AS POSIÇÕES DO ARRAY */
+        numero[0] = 15;
+        numero[1] = 6;
+        numero[2] = 7;
+        numero[3] = 9;
+        numero[4] = 10;
+        numero[5] = 4;
+        numero[6] = 3;
+        numero[7] = 1;
+        numero[8] = 0;
+        numero[9] = 12;
+
     }
 }
