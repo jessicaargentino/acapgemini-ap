094bd6bcaf60907c4330c236acee888e103b9017
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-10-04 13:42:49 -0300

    A17 - Atribuição de valores com os setters

--- src/modulo02/aula17/TelaPrincipalLoja.java
+++ src/modulo02/aula17/TelaPrincipalLoja.java
@@ -4,5 +4,12 @@ public class TelaPrincipalLoja {
     public static void main(String[] args) {
         /* CRIAÇÃO DE UM OBJETO ATRAVÉS DA INSTÂNCIA DA CLASSE PRODUTO */
         Produto produto = new Produto();
+
+        /* ATRIBUIÇÃO DE VALORES UTILIZANDO OS SETTERS */
+        produto.setCodigo(18365);
+        produto.setNome("Kalimba");
+        produto.setDescricao("Instrumento musical");
+        produto.setValor(80.00);
+    
     }
 }
