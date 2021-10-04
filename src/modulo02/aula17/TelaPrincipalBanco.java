4e6cf8ef136ca5ed05f4a5119bfd214a84f7d855
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-10-04 13:50:45 -0300

    A17 - Atribuição de valores utilizando os setters

--- src/modulo02/aula17/TelaPrincipalBanco.java
+++ src/modulo02/aula17/TelaPrincipalBanco.java
@@ -4,5 +4,12 @@ public class TelaPrincipalBanco {
     public static void main(String[] args) {
         /* CRIAÇÃO DE UM OBJETO ATRAVÉS DA INSTÂNCIA DA CLASSE CONTACORRENTE */
         ContaCorrente conta = new ContaCorrente();
+
+        /* ATRIBUIÇÃO DE VALORES UTILIZANDO OS SETTERS */
+        conta.setAgencia(8786);
+        conta.setNumero(873361);
+        conta.setCodigoCliente(9876);
+        conta.setSaldo(6500.00);
+
     }
 }
