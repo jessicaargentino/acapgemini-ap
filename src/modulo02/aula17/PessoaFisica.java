d30664ed39cee060232585f4f431dd1fe7d057d8
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-10-04 17:26:11 -0300

    A17 - Declaração dos atributos

--- src/modulo02/aula17/PessoaFisica.java
+++ src/modulo02/aula17/PessoaFisica.java
@@ -1,5 +1,11 @@
 package modulo02.aula17;
 
 public class PessoaFisica {
-    
+    /* DECLARAÇÃO DOS ATRIBUTOS DA CLASSE */
+    private int codigo;
+    private String nome;
+    private String sobrenome;
+    private short idade;
+    private String rg;
+    private String cpf;
 }
