1c6192d352c506875f9cc09cf0fdc53dc9155363
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-10-05 15:22:26 -0300

    A18 - Definição dos atributos

--- src/modulo02/aula18/Pessoa.java
+++ src/modulo02/aula18/Pessoa.java
@@ -2,5 +2,8 @@ package modulo02.aula18;
 
 /* CLASSE PAI*/
 public class Pessoa {
-
+    /* DEFINIÇÃO DOS ATRIBUTOS */
+    private String nome;
+    private String sobrenome;
+    private int matricula;
 }
