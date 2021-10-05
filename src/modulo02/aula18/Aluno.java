dc10b186fe1ba4a8f58e4e3c3fd278347090ea0c
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-10-05 15:32:13 -0300

    A18 - Declaração dos getters e setters

--- src/modulo02/aula18/Aluno.java
+++ src/modulo02/aula18/Aluno.java
@@ -5,4 +5,21 @@ public class Aluno {
     /* DEFINIÇÃO DOS ATRIBUTOS */
     private String curso;
     private String turma;
+
+    /* GETTERS E SETTERS */
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setTurma(String turma) {
+        this.turma = turma;
+    }
+
+    public String getTurma() {
+        return turma;
+    }
 }
