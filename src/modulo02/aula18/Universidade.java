0bca043ea169340b2192ee4dfbe31aca4ad2786d
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-10-05 15:37:44 -0300

    A18 - Instância de Aluno, Coordenador, Funcionario

--- src/modulo02/aula18/Universidade.java
+++ src/modulo02/aula18/Universidade.java
@@ -2,7 +2,13 @@ package modulo02.aula18;
 
 public class Universidade {
     public static void main(String[] args) {
-
+        /*
+         * CRIAÇÃO DE TRÊS OBJETOS ATRAVÉS DA INSTÂNCIA DAS CLASSES ALUNO, COORDENADOR E
+         * FUNCIONARIO
+         */
+        Aluno aluno = new Aluno();
+        Coordenador coordenador = new Coordenador();
+        Funcionario funcionario = new Funcionario();
     }
 
 }
