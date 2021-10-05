575f51c72e8984ed47f133894251fde1b5c8d4c9
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-10-05 15:28:29 -0300

    A18 - Definição dos atributos

--- src/modulo02/aula18/Coordenador.java
+++ src/modulo02/aula18/Coordenador.java
@@ -2,5 +2,7 @@ package modulo02.aula18;
 
 /* CLASSE FILHA DE FUNCIONARIO */
 public class Coordenador {
-
+    /* DEFINIÇÃO DOS ATRIBUTOS */
+    private String cursoCoordenado;
+    private String bonus;
 }
