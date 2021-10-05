aaeddb12b577b9359466fc182080487edc331643
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-10-05 15:29:00 -0300

    A18 - Declaração dos getters e setters

--- src/modulo02/aula18/Coordenador.java
+++ src/modulo02/aula18/Coordenador.java
@@ -5,4 +5,21 @@ public class Coordenador {
     /* DEFINIÇÃO DOS ATRIBUTOS */
     private String cursoCoordenado;
     private String bonus;
+
+    /* GETTERS E SETTERS */
+    public void setCursoCoordenado(String cursoCoordenado) {
+        this.cursoCoordenado = cursoCoordenado;
+    }
+
+    public String getCursoCoordenado() {
+        return cursoCoordenado;
+    }
+
+    public void setBonus(String bonus) {
+        this.bonus = bonus;
+    }
+
+    public String getBonus() {
+        return bonus;
+    }
 }
