aa8d690b2e864dcc29772337e03d1fa64249ce81
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-10-05 15:26:30 -0300

    A18 - Declaração dos getters e setters

--- src/modulo02/aula18/Funcionario.java
+++ src/modulo02/aula18/Funcionario.java
@@ -6,4 +6,20 @@ public class Funcionario {
     private double salario;
     private String departamento;
 
+    /* GETTERS E SETTERS */
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setDepartamento(String departamento) {
+        this.departamento = departamento;
+    }
+
+    public String getDepartamento() {
+        return departamento;
+    }
 }
