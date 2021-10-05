72e4a2af69df00e20e4598570a833404146da54a
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-10-05 15:25:48 -0300

    A18 - Definição dos atributos

--- src/modulo02/aula18/Funcionario.java
+++ src/modulo02/aula18/Funcionario.java
@@ -2,5 +2,8 @@ package modulo02.aula18;
 
 /* CLASSE FILHA DE PESSOA */
 public class Funcionario {
+    /* DEFINIÇÃO DOS ATRIBUTOS */
+    private double salario;
+    private String departamento;
 
 }
