52043f6db86eaa1cddb90df33f7ecfe6b7fa3b9d
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-10-06 13:17:31 -0300

    A19 - ContaCorrente herda Conta

--- src/modulo02/aula19/ContaCorrente.java
+++ src/modulo02/aula19/ContaCorrente.java
@@ -1,7 +1,7 @@
 package modulo02.aula19;
 
 /* CLASSE FILHA DE CONTA */
-public class ContaCorrente {
+public class ContaCorrente extends Conta {
     /* DEFINIÇÃO DOS ATRIBUTOS */
     private double taxaTransferencia = 10.50;
     private double taxaManuntencao = 2.50;
