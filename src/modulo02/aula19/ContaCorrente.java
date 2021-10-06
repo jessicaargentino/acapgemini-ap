12a40a36ed21408ff937755b42f453cb8d087c6e
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-10-06 13:15:52 -0300

    A19 - Definição dos atributos

--- src/modulo02/aula19/ContaCorrente.java
+++ src/modulo02/aula19/ContaCorrente.java
@@ -2,5 +2,8 @@ package modulo02.aula19;
 
 /* CLASSE FILHA DE CONTA */
 public class ContaCorrente {
-
+    /* DEFINIÇÃO DOS ATRIBUTOS */
+    private double taxaTransferencia;
+    private double taxaManuntencao;
+    private int numeroDeTransferencia;
 }
