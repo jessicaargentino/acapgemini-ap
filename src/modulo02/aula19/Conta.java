38f051f86900f52541dce3cdc27a2adbc36f8e19
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-10-06 13:09:39 -0300

    A19 - Declaração dos getters e setters

--- src/modulo02/aula19/Conta.java
+++ src/modulo02/aula19/Conta.java
@@ -5,4 +5,21 @@ public class Conta {
     /* DEFINIÇÃO DOS ATRIBUTOS */
     private double saldo;
     private int codigoCliente;
+
+    /* GETTERS E SETTERS */
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setCodigoCliente(int codigoCliente) {
+        this.codigoCliente = codigoCliente;
+    }
+
+    public int getCodigoCliente() {
+        return codigoCliente;
+    }
 }
