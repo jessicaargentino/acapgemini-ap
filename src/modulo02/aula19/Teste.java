610be943728af8f3f43641292bef55a218eda3a9
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-10-06 13:28:35 -0300

    A19 - Atribuição de valores utilizando setter

--- src/modulo02/aula19/Teste.java
+++ src/modulo02/aula19/Teste.java
@@ -8,6 +8,11 @@ public class Teste {
          */
         Conta conta = new Conta();
         ContaCorrente cc = new ContaCorrente();
-        
+
+        /* ATRIBUIÇÃO DE VALORES PARA CONTA E CC UTILIZANDO SETTER */
+        conta.setCodigoCliente(120004);
+        conta.setSaldo(1000.00);
+        cc.setCodigoCliente(198970);
+        cc.setSaldo(1500.00);
     }
 }
