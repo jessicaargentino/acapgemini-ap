cb43de3567b2decc2b05d3dfcbf373ddf92238f4
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-10-06 13:55:40 -0300

    A19 - Método debitaTaxaManutencao

--- src/modulo02/aula19/ContaCorrente.java
+++ src/modulo02/aula19/ContaCorrente.java
@@ -4,14 +4,31 @@ package modulo02.aula19;
 public class ContaCorrente extends Conta {
     /* DEFINIÇÃO DOS ATRIBUTOS */
     private double taxaTransferencia = 10.50;
-    private double taxaManuntencao = 2.50;
+    private double taxaManutencao = 2.50;
     private int numeroDeTransferencia;
 
+    /*
+     * MÉTODO SOBREESCRITO QUE ALÉM DE DEBITAR O VALOR DO SALDO ATUAL, DEBITA A TAXA
+     * DE TRANSFERÊNCIA TAMBÉM.
+     */
     @Override
     public double realizaTransferencia(double valor) {
         numeroDeTransferencia++;
         double saldo = this.getSaldo();
         this.setSaldo(saldo - taxaTransferencia);
         return valor;
+    }
+
+    /*
+     * MÉTODO QUE VERIFICA SE FORAM REALIZADOS GRUPOS DE 5 TRANSFERÊNCIAS. SE SIM,
+     * DEBITA DO SALDO ATUAL A TAXA DE MANUTENÇÃO, SE NÃO, APENAS RETORNA O SALDO
+     * ATUAL.
+     */
+    public double debitaTaxaManutencao() {
+        if (numeroDeTransferencia % 5 == 0) {
+            return (this.getSaldo() - taxaManutencao);
+        } else {
+            return this.getSaldo();
+        }
     }
 }
