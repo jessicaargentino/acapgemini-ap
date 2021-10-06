321aaea5ba207687770590efeb8dd0c44bd98928
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-10-06 13:27:01 -0300

    A19 - Instanciação de Conta e ContaCorrente

--- src/modulo02/aula19/Teste.java
+++ src/modulo02/aula19/Teste.java
@@ -2,6 +2,12 @@ package modulo02.aula19;
 
 public class Teste {
     public static void main(String[] args) {
+        /*
+         * CRIAÇÃO DE DOIS OBJETOS ATRAVÉS DA INSTÂNCIA DAS CLASSES CONTA E
+         * CONTACORRENTE
+         */
+        Conta conta = new Conta();
+        ContaCorrente cc = new ContaCorrente();
         
     }
 }
