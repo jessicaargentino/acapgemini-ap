c730e0a581458795bc48ca6126a8027df0e91f31
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-10-06 13:39:05 -0300

    A19 - Impressão dos atributos

--- src/modulo02/aula19/Teste.java
+++ src/modulo02/aula19/Teste.java
@@ -12,8 +12,10 @@ public class Teste {
         /* ATRIBUIÇÃO DE VALORES PARA CONTA E CC UTILIZANDO SETTER */
         conta.setCodigoCliente(120004);
         conta.setSaldo(1000.00);
+        double saldoConta = conta.getSaldo();
         cc.setCodigoCliente(198970);
         cc.setSaldo(1500.00);
+        double saldoCc = cc.getSaldo();
 
         /* CHAMADA DO MÉTODO DE TRANSFERÊNCIA PARA CADA OBJETO */
         conta.realizaTransferencia(500.00);
@@ -21,5 +23,12 @@ public class Teste {
 
         /* IMPRESSÃO DE CABEÇALHO */
         System.out.println("***** CAPBANCO *****");
+        System.out.printf("\nConta - código cliente: %d\n", conta.getCodigoCliente());
+        System.out.printf("Saldo prévio: R$%.2f\n", saldoConta);
+        System.out.printf("Saldo atual pós transferência: R$%.2f\n", conta.getSaldo());
+
+        System.out.printf("\nConta Corrente - código cliente: %d\n", cc.getCodigoCliente());
+        System.out.printf("Saldo prévio: R$%.2f\n", saldoCc);
+        System.out.printf("Saldo atual pós transferência: R$%.2f\n", cc.getSaldo());
     }
 }
