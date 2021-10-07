025d3d2924170f65ba1694a80a1197909c910552
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-10-07 15:56:02 -0300

    A20 - Instanciação da classe Aluno em dois objetos

--- src/modulo02/aula20/atv01/Tela.java
+++ src/modulo02/aula20/atv01/Tela.java
@@ -2,6 +2,10 @@ package modulo02.aula20.atv01;
 
 public class Tela {
     public static void main(String[] args) {
-        
-    }   
+        /*
+         * CRIAÇÃO DE DOIS OBJETOS ATRAVÉS DA INSTÂNCIA DA CLASSE ALUNO (OBJETO)
+         */
+        Aluno aluno1 = new Aluno();
+        Aluno aluno2 = new Aluno();
+    }
 }
