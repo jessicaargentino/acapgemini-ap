63b2f5bbcb9941eb84452e3a3b66bea3b3b88cc2
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-10-07 16:12:05 -0300

    A20 - Atribuição de valor para taxaSaque

--- src/modulo02/aula20/atv02/ContaCorrente.java
+++ src/modulo02/aula20/atv02/ContaCorrente.java
@@ -3,7 +3,7 @@ package modulo02.aula20.atv02;
 public class ContaCorrente {
     /* DEFINIÇÃO DOS ATRIBUTOS */
     private double saldo;
-    private double taxaSaque;
+    private double taxaSaque = 1.50;
     private int codigoCliente;
 
     /* DEFINIÇÃO DO MÉTODO CONSTRUTOR DA CLASSE */
