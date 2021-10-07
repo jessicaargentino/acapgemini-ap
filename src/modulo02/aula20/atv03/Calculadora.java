3b42f0f725543fc1a624e5fe6d8cf9bcbbc76c6d
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-10-07 16:32:09 -0300

    A20 - Método de soma de inteiros

--- src/modulo02/aula20/atv03/Calculadora.java
+++ src/modulo02/aula20/atv03/Calculadora.java
@@ -1,5 +1,8 @@
 package modulo02.aula20.atv03;
 
 public class Calculadora {
-
+    /* MÉTODO QUE RECEBE DOIS VALORES INTEIROS E RETORNA A SOMA ENTRE ELES */
+    public int soma(int num1, int num2) {
+        return num1 + num2;
+    }
 }
