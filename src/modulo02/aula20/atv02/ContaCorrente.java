1c06f04374bbb810eeb7fb02643b9538aff71fc0
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-10-07 16:04:09 -0300

    A20 - Definição dos atributos

--- src/modulo02/aula20/atv02/ContaCorrente.java
+++ src/modulo02/aula20/atv02/ContaCorrente.java
@@ -1,5 +1,9 @@
 package modulo02.aula20.atv02;
 
 public class ContaCorrente {
-    
+    /* DEFINIÇÃO DOS ATRIBUTOS */
+    private double saldo;
+    private double taxaSaque;
+    private int codigoCliente;
+
 }
