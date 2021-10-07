4e5f0a85708792729aa69fd7680f2cfab4a9da3b
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-10-07 16:38:10 -0300

    A20 - Instanciação da classe Calculadora

--- src/modulo02/aula20/atv03/Tela.java
+++ src/modulo02/aula20/atv03/Tela.java
@@ -2,6 +2,9 @@ package modulo02.aula20.atv03;
 
 public class Tela {
     public static void main(String[] args) {
-
+        /*
+         * CRIAÇÃO DE UM OBJETO ATRAVÉS DA INSTÂNCIA DA CLASSE CALCULADORA
+         */
+        Calculadora calculadora = new Calculadora();
     }
 }
