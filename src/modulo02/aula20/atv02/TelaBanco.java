a55b7f14742dc07bda6c7330243cc1df0bf46fd2
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-10-07 16:25:58 -0300

    A20 - Chamada dos métodos e impressão

--- src/modulo02/aula20/atv02/TelaBanco.java
+++ src/modulo02/aula20/atv02/TelaBanco.java
@@ -2,6 +2,18 @@ package modulo02.aula20.atv02;
 
 public class TelaBanco {
     public static void main(String[] args) {
-        
+        /*
+         * CRIAÇÃO DE UM OBJETO ATRAVÉS DA INSTÂNCIA DA CLASSE CONTACORRENTE
+         */
+        ContaCorrente conta = new ContaCorrente(1500.0, 234567);
+
+        /* CHAMADA DO MÉTODO DE REALIZAR DEPÓSITO */
+        conta.recebeDeposito(500.0);
+
+        /* CHAMADA DO MÉTODO DE REALIZAR SAQUE */
+        conta.realizaSaque(1000.0);
+
+        /* IMPRESSÃO ATRAVÉS DO TOSTRING */
+        System.out.println(conta);
     }
 }
