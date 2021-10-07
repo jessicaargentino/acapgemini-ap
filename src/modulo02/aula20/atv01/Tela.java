ec191a2d941c23d1470f127077ab2c89b62c5a76
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-10-07 16:48:24 -0300

    A20 - Correção do comentário

--- src/modulo02/aula20/atv01/Tela.java
+++ src/modulo02/aula20/atv01/Tela.java
@@ -3,7 +3,7 @@ package modulo02.aula20.atv01;
 public class Tela {
     public static void main(String[] args) {
         /*
-         * CRIAÇÃO DE DOIS OBJETOS ATRAVÉS DA INSTÂNCIA DA CLASSE ALUNO (OBJETO)
+         * CRIAÇÃO DE DOIS OBJETOS ATRAVÉS DA INSTÂNCIA DA CLASSE ALUNO
          */
         Aluno aluno1 = new Aluno();
         Aluno aluno2 = new Aluno();
