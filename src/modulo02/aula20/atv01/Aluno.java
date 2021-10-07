9f260c2947e8d8c98582e7bef79f28f479fbfa5c
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-10-07 15:51:59 -0300

    A20 - Declaração dos getters e setters

--- src/modulo02/aula20/atv01/Aluno.java
+++ src/modulo02/aula20/atv01/Aluno.java
@@ -5,4 +5,29 @@ public class Aluno {
     private String nomeCompleto;
     private int idade;
     private int matricula;
+
+    /* GETTERS E SETTERS */
+    public void setNomeCompleto(String nomeCompleto) {
+        this.nomeCompleto = nomeCompleto;
+    }
+
+    public String getNomeCompleto() {
+        return this.nomeCompleto;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public int getIdade() {
+        return this.idade;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    public int getMatricula() {
+        return matricula;
+    }
 }
