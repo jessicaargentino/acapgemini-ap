acbdd82485ae11ebb83983fc430dc4a91c1618f3
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-10-07 16:34:16 -0300

    A20 - Método soma de doubles

--- src/modulo02/aula20/atv03/Calculadora.java
+++ src/modulo02/aula20/atv03/Calculadora.java
@@ -5,4 +5,9 @@ public class Calculadora {
     public int soma(int num1, int num2) {
         return num1 + num2;
     }
+
+    /* MÉTODO QUE RECEBE DOIS VALORES DOUBLE E RETORNA A SOMA ENTRE ELES */
+    public double soma(double num1, double num2) {
+        return num1 + num2;
+    }
 }
