fe9cbcc99a105892d75dab3b1837d0e4a259a9b8
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-10-07 15:59:30 -0300

    A20 - Atribuição de valores e impressão dos mesmos

--- src/modulo02/aula20/atv01/Tela.java
+++ src/modulo02/aula20/atv01/Tela.java
@@ -7,5 +7,19 @@ public class Tela {
          */
         Aluno aluno1 = new Aluno();
         Aluno aluno2 = new Aluno();
+
+        /*
+         * ATRIBUIÇÃO DE VALORES UTILIZANDO OS SETTERS DE CADA OBJETO E IMPRESSÃO
+         * UTILIZANDO O TOSTRING
+         */
+        aluno1.setNomeCompleto("Laura Palmer");
+        aluno1.setIdade(17);
+        aluno1.setMatricula(123789);
+        System.out.println(aluno1);
+
+        aluno2.setNomeCompleto("Luke Skywalker");
+        aluno2.setIdade(53);
+        aluno2.setMatricula(890573);
+        System.out.println(aluno2);
     }
 }
