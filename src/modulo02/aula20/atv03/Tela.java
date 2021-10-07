2abc1888b1fca613dc8cf3a3b140311ea4bc0393
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-10-07 16:47:45 -0300

    A20 - Chamada dos métodos e impressão

--- src/modulo02/aula20/atv03/Tela.java
+++ src/modulo02/aula20/atv03/Tela.java
@@ -6,5 +6,11 @@ public class Tela {
          * CRIAÇÃO DE UM OBJETO ATRAVÉS DA INSTÂNCIA DA CLASSE CALCULADORA
          */
         Calculadora calculadora = new Calculadora();
+
+        /* CHAMADA E IMPRESSÃO DE CADA MÉTODO DE SOMA */
+        System.out.println("Soma de inteiros: " + calculadora.soma(5, 2));
+        System.out.println("Soma de doubles: " + calculadora.soma(5.0, 2.0));
+        System.out.println("Soma de floats: " + calculadora.soma(4.0f, 1.0f, 2.0f));
+        System.out.println("Soma de longs: " + calculadora.soma(4L, 1L, 2L));
     }
 }
