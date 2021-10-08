33f66c760c0b9d657a4f3e0e7371e6dab92f5aa4
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-10-08 12:04:23 -0300

    A17 - Inclusão de enunciado

--- src/modulo02/aula17/PessoaFisica.java
+++ src/modulo02/aula17/PessoaFisica.java
@@ -1,5 +1,14 @@
 package modulo02.aula17;
 
+/* Crie uma classe Pessoa Física com os atributos código, nome, sobrenome,
+* idade, rg e cpf. Todos os atributos da classe Pessoa Física devem ser
+* privados. Construa os métodos get e set para cada um dos atributos da
+* classe. Crie uma classe Cadastro de Pessoas que contenha um método main.
+* Nesta classe, crie um array de Pessoa com 5 posições. Crie 5 objetos da
+* classe pessoa, chame os método set para atribuir valores diferente para cada
+* pessoa e salve cada objeto em uma posição do array. Imprima todos os dados
+* das 5 pessoas utilizando o laço de repetição foreach.*/
+
 public class PessoaFisica {
     /* DECLARAÇÃO DOS ATRIBUTOS DA CLASSE */
     private int codigo;
