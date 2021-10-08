7fadd958c067523bad533f120ececb0c0798955e
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-10-08 12:09:11 -0300

    A16 - Inclusão do enunciado

--- src/modulo02/aula16/CalculoImposto.java
+++ src/modulo02/aula16/CalculoImposto.java
@@ -1,5 +1,15 @@
 package modulo02.aula16;
 
+/* Crie uma classe para cálculo de impostos. A classe de calculo deve possuir
+* 3 métodos, um para calculo de ISS, outro para cálculo de IOF e outro para
+* cálculo de IR. A classe deve manter em variáveis privadas o valor de cada
+* imposto que deve ser definido por você. Os métodos devem receber como
+* parâmetro um valor que servirá de base para cálculo do imposto e retornar o
+* valor do imposto calculado. Crie uma segunda classe que contenha o método
+* main. Nesta segunda classe, crie um objeto da classe calculadora, realize a
+* chamada aos três métodos e ao final da execução, imprima o valor de todos os
+* impostos calculados. */
+
 public class CalculoImposto {
     /* DEFINIÇÃO DOS ATRIBUTOS DA CLASSE */
     private double iss = 0.078; // 7.8%
