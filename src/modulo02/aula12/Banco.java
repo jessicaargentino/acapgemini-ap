eb16e9b2c263fa96206b115f68e8032a76bbdc34
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-10-08 12:19:25 -0300

    A12 - Inclusão do enunciado

--- src/modulo02/aula12/Banco.java
+++ src/modulo02/aula12/Banco.java
@@ -2,8 +2,24 @@ package modulo02.aula12;
 
 import java.util.Scanner;
 
-public class Banco {
+/* Crie um programa em linguagem Java que atenda aos seguintes requisitos:
+* Exiba um cabeçalho com o título “Bradesco Financiamentos”. Exiba um menu
+* com as opções, Crédito Pessoal, Crédito Imobiliário, Crédito Empresarial e
+* Empréstimo Consignado. Solicite ao usuário que escolha uma das opções do
+* menu e caso a opção digitada não seja válida, o sistema deve imprimir o menu
+* novamente, solicitar que ele digite a opção e validar a resposta até que uma
+* opção válida seja digitada. Ao digitar uma opção válida o sistema deve
+* exibir apenas uma mensagem com o nome do módulo, nenhuma implementação da
+* funcionalidade do módulo será necessária. Após exibir a opção correta, o
+* sistema deve perguntar ao usuário se ele deseja voltar ao menu principal
+* (‘V’) ou sair do sistema(‘S’). O sistema deve ler a opção digitada pelo
+* usuário e validar se ele digitou uma das opções válidas, caso contrário deve
+* solicitar que digite novamente até que uma opção válida seja digitada. O
+* sistema deve encaminhar o usuário para o menu principal caso a resposta seja
+* ‘V’ e se a opção digitada for ‘S’ o sistema deve encerrar imprimindo uma
+* mensagem de obrigado. */
 
+public class Banco {
     static Scanner entrada = new Scanner(System.in);
 
     public static void main(String[] args) {
