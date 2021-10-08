91e3acf8df1d132f2be19026b03552aed533d0f7
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-10-08 12:11:35 -0300

    A15 - Inclusão do enunciado

--- src/modulo02/aula15/CalculadoraTaxas.java
+++ src/modulo02/aula15/CalculadoraTaxas.java
@@ -1,5 +1,15 @@
 package modulo02.aula15;
 
+/* Crie uma nova classe para esta atividade e que possua um método main para
+* executar a aplicação. Construa uma classe CalculadoraTaxas que possua os
+* métodos de cálculo de taxas. Crie dois métodos para cálculo de taxa sobre
+* transações bancárias, o primeiro será aplicado uma taxa de 0,001% sobre o
+* valor transferido, o segundo uma taxa de saque de R$1.30 a cada 5 saques
+* realizados. Na classe principal, dentro do metodo main, construa um objeto da
+* classe Calculadora de Taxas e execute os métodos de transferência e de saque.
+* Ao final da execução do método main, imprima todos os dados das taxas
+* cobradas. */
+
 public class CalculadoraTaxas {
     // DEFINIÇÃO DOS ATRIBUTOS DA CLASSE
     private double taxaTransferencia;
