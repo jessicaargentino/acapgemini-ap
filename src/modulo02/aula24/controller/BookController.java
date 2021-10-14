91b1e479e34a3b2cd358883697ad1a8cc9a0c663
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-10-14 16:12:45 -0300

    A24 - Declaração do ArrayList de Book

--- src/modulo02/aula24/controller/BookController.java
+++ src/modulo02/aula24/controller/BookController.java
@@ -1,5 +1,9 @@
 package modulo02.aula24.controller;
 
+import java.util.ArrayList;
+
+import modulo02.aula24.model.Book;
+
 public class BookController {
-    
+    private ArrayList<Book> book = new ArrayList<Book>();
 }
