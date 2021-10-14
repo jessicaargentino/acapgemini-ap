a28498c2fc452aef8123563c228fe3e3c1ccc417
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-10-14 16:06:32 -0300

    A24 - Construtor com incremento do id

--- src/modulo02/aula24/model/BaseModel.java
+++ src/modulo02/aula24/model/BaseModel.java
@@ -2,4 +2,9 @@ package modulo02.aula24.model;
 
 public class BaseModel {
     private int id;
+
+    /** Construtor para incrementação do id a cada instância */
+    public BaseModel() {
+        this.id++;
+    }
 }
