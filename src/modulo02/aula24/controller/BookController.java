6832aa34654df357418f3ea1d3aecc5668fc1de9
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-10-14 16:15:33 -0300

    A24 - Crud create

--- src/modulo02/aula24/controller/BookController.java
+++ src/modulo02/aula24/controller/BookController.java
@@ -6,4 +6,13 @@ import modulo02.aula24.model.Book;
 
 public class BookController {
     private ArrayList<Book> book = new ArrayList<Book>();
+
+    /**
+     * CRUD - Método CREATE: Criar um objeto de Categoria
+     * 
+     * @param b objeto da classe Book
+     */
+    public void create(Book b) {
+        this.book.add(b);
+    }
 }
