eadda7f3d46b807775998d4ec352e26b3fd1345c
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-10-14 15:58:47 -0300

    A24 - Definição dos atributos

--- src/modulo02/aula24/model/Book.java
+++ src/modulo02/aula24/model/Book.java
@@ -1,5 +1,9 @@
 package modulo02.aula24.model;
 
 public class Book {
-
+    private String name;
+    private String description;
+    private String author;
+    private String publisher;
+    private int yearOfPublication;
 }
