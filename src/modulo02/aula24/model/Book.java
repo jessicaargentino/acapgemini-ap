bd40ac2cb99dd699261e34d376f7463f9feccb69
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-10-14 16:08:38 -0300

    A24 - Declaração dos getters e setters

--- src/modulo02/aula24/model/Book.java
+++ src/modulo02/aula24/model/Book.java
@@ -1,9 +1,50 @@
 package modulo02.aula24.model;
 
-public class Book {
+public class Book extends BaseModel {
     private String name;
     private String description;
     private String author;
     private String publisher;
     private int yearOfPublication;
+
+    /** Getters e setters */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setYearOfPublication(int yearOfPublication) {
+        this.yearOfPublication = yearOfPublication;
+    }
+
+    public int getYearOfPublication() {
+        return yearOfPublication;
+    }
 }
