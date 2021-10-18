330c48a2cf89e4abb0b4d0313eef794953957fcb
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-10-18 14:11:13 -0300

    A26 - Instanciação das classes

--- src/modulo02/aula26/Main.java
+++ src/modulo02/aula26/Main.java
@@ -2,6 +2,8 @@ package modulo02.aula26;
 
 public class Main {
     public static void main(String[] args) {
-
+        /** Criação de dois objetos através das instâncias da classe Vehicle e Car */
+        Vehicle vehicle = new Vehicle();
+        Car car = new Car();
     }
 }
