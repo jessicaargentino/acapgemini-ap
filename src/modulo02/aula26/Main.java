afc494a629c1e1d25b20b0a38ba43d10f1d50361
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-10-18 14:13:27 -0300

    A26 - Objeto de Object através da classe Car

--- src/modulo02/aula26/Main.java
+++ src/modulo02/aula26/Main.java
@@ -5,5 +5,12 @@ public class Main {
         /** Criação de dois objetos através das instâncias da classe Vehicle e Car */
         Vehicle vehicle = new Vehicle();
         Car car = new Car();
+
+        /**
+         * Criação de dois objetos de Object através da classe Vehicle e Car
+         * respectivamente
+         */
+        Object objectOne = vehicle;
+        Object objectTwo = car;
     }
 }
