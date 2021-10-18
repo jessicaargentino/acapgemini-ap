75c87a3e919bde8fbad6df4e1abb0a57a6199f85
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-10-18 16:41:52 -0300

    A26 - Definição dos atributos

--- src/modulo02/aula26/ap02/Notebook.java
+++ src/modulo02/aula26/ap02/Notebook.java
@@ -1,5 +1,7 @@
 package modulo02.aula26.ap02;
 
-public class Notebook {
-    
+public class Notebook extends Computer{
+    public String batery;
+    public String wifiSpeed;
+    public double bluetoothVersion;
 }
