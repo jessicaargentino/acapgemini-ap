04ed90f6ce47d9a304286d87a8382153c3d8d732
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-10-18 14:33:01 -0300

    A26 - Sobreescrita do toString

--- src/modulo02/aula26/Car.java
+++ src/modulo02/aula26/Car.java
@@ -3,4 +3,9 @@ package modulo02.aula26;
 public class Car {
     public int numberOfDoors;
     public String capacityOfTrunk;
+
+    @Override
+    public String toString() {
+        return "\nNúmero de portas: " + this.numberOfDoors + "\nCapacidade porta-malas: " + this.capacityOfTrunk;
+    }
 }
