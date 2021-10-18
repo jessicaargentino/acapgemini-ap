07d8e3c6d169948dc9abad0d44c0a7f4927658aa
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-10-18 14:40:31 -0300

    A26 - Correção da classe

--- src/modulo02/aula26/Car.java
+++ src/modulo02/aula26/Car.java
@@ -3,9 +3,4 @@ package modulo02.aula26;
 public class Car {
     public int numberOfDoors;
     public String capacityOfTrunk;
-
-    @Override
-    public String toString() {
-        return "\nNúmero de portas: " + this.numberOfDoors + "\nCapacidade porta-malas: " + this.capacityOfTrunk;
-    }
 }
--- src/modulo02/aula26/Vehicle.java
+++ src/modulo02/aula26/Vehicle.java
@@ -4,9 +4,4 @@ public class Vehicle {
     public String brand;
     public String model;
     public String plate;
-
-    @Override
-    public String toString() {
-        return "\nMarca: " + this.brand + "\nModelo: " + this.model + "\nPlaca: " + this.plate;
-    }
 }
