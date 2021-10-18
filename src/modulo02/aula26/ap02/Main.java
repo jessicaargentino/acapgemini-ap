4ff1ff075440214bcba92908af1ef3a3b4e5d5c2
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-10-18 16:51:07 -0300

    A26 - Instanciação das classes

--- src/modulo02/aula26/ap02/Main.java
+++ src/modulo02/aula26/ap02/Main.java
@@ -2,6 +2,12 @@ package modulo02.aula26.ap02;
 
 public class Main {
     public static void main(String[] args) {
-        
+        /**
+         * Criação de três objetos através das instâncias das classes Computer, Desktop
+         * e Notebook
+         */
+        Computer computer = new Computer();
+        Notebook notebook = new Notebook();
+        Desktop desktop = new Desktop();
     }
 }
