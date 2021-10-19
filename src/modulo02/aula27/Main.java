124f88fa9620fcb2f9aff665ac82995d12bdffe8
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-10-19 15:13:29 -0300

    A27 - Instâncias e atribuição de valores

--- src/modulo02/aula27/Main.java
+++ src/modulo02/aula27/Main.java
@@ -2,6 +2,20 @@ package modulo02.aula27;
 
 public class Main {
     public static void main(String[] args) {
+        /** Objetos das classes People e Student */
+        People people = new People();
+        Student student = new Student();
 
+        /** Atribuição de valores */
+        people.name = "Laura";
+        people.lastName = "Palmer";
+        people.age = 17;
+
+        student.name = "Laura";
+        student.lastName = "Palmer";
+        student.age = 17;
+        student.registration = 908653;
+        student.group = "Turma D";
+        student.course = "Sistemas de informação";
     }
 }
