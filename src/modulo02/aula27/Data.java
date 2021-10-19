9adfa51b76c222adeab40dfa0e5e0e43facc5fce
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-10-19 14:59:59 -0300

    A27 - Método add

--- src/modulo02/aula27/Data.java
+++ src/modulo02/aula27/Data.java
@@ -2,8 +2,34 @@ package modulo02.aula27;
 
 public class Data {
     private Object[] data;
+    private int currentPosition;
+    private int capacity;
 
+    /**
+     * Construtor Inicializa a variável capacidade com tamanho 5 e atribui a
+     * variável como tamanho do array de Object
+     */
     public Data() {
-        this.data = new Object[5];
+        this.capacity = 5;
+        this.data = new Object[this.capacity];
+    }
+
+    /**
+     * Método que adiciona um objeto no Array de Objects
+     * 
+     * @param object objeto de uma Classe
+     */
+    public void add(Object object) {
+        if (currentPosition < data.length) {
+            data[currentPosition] = object;
+            currentPosition++;
+        } else {
+            capacity += 5;
+            Object[] dataNew = new Object[capacity];
+            for (int n = 0; n < data.length; n++) {
+                dataNew[n] = data[n];
+            }
+            data = dataNew;
+        }
     }
 }
