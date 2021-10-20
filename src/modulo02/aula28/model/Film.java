a88244567df767ca1133b162d945b65a53d820e3
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-10-20 16:38:55 -0300

    A28 - Atualização das classes

--- src/modulo02/aula28/model/Film.java
+++ src/modulo02/aula28/model/Film.java
@@ -1,34 +1,10 @@
 package modulo02.aula28.model;
 
-public class Film {
-    public int id;
-    public static int counter = 1;
-    public String name;
-    public String genre;
-    public int releaseYear;
+public class Film extends Audiovisual {
     public String direction;
-
-    /**
-     * Construtor O id é incrementado a cada instância da classe
-     */
-    public Film() {
-        this.id = counter++;
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        if (object instanceof Film) {
-            Film film = (Film) object;
-            if (this.id == film.id) {
-                return true;
-            }
-        }
-        return false;
-    }
 
     @Override
     public String toString() {
-        return "\nID: " + this.id + "\nNome: " + this.name + "\nGênero: " + this.genre + "\nAno de lançamento: "
-                + this.releaseYear + "\nDireção: " + this.direction;
+        return super.toString() + "\nDireção: " + this.direction;
     }
 }
--- src/modulo02/aula28/model/Series.java
+++ src/modulo02/aula28/model/Series.java
@@ -1,34 +1,10 @@
 package modulo02.aula28.model;
 
-public class Series {
-    public int id;
-    public static int counter = 1;
-    public String name;
-    public String genre;
-    public int releaseYear;
+public class Series extends Audiovisual {
     public int season;
-
-    /**
-     * Construtor O id é incrementado a cada instância da classe
-     */
-    public Series() {
-        this.id = counter++;
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        if (object instanceof Series) {
-            Series series = (Series) object;
-            if (this.id == series.id) {
-                return true;
-            }
-        }
-        return false;
-    }
 
     @Override
     public String toString() {
-        return "\nID: " + this.id + "\nNome: " + this.name + "\nGênero: " + this.genre + "\nAno de lançamento: "
-                + this.releaseYear + "\nTemporadas: " + this.season;
+        return super.toString() + "\nTemporadas: " + this.season;
     }
 }
