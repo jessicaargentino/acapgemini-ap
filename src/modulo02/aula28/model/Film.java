bbee30c3038f94a156731cbe7f684ca5c6db1a8b
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-10-20 15:42:56 -0300

    A28 - Método equals e toString

--- src/modulo02/aula28/model/Film.java
+++ src/modulo02/aula28/model/Film.java
@@ -1,8 +1,33 @@
 package modulo02.aula28.model;
 
 public class Film {
-    private String name;
-    private String genre;
-    private int releaseYear;
-    private String direction;
+    public int id;
+    public String name;
+    public String genre;
+    public int releaseYear;
+    public String direction;
+
+    /**
+     * Construtor O id é incrementado a cada instância da classe
+     */
+    public Film() {
+        this.id++;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (object instanceof Film) {
+            Film film = (Film) object;
+            if (this.id == film.id) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "\nNome: " + this.name + "\nGênero: " + this.genre + "\nAno de lançamento: " + this.releaseYear
+                + "\nDireção: " + this.direction;
+    }
 }
