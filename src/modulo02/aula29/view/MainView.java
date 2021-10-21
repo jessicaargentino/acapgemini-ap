1ec8bb8f1d5468e4e2d6687d961f5a17d86c50de
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-10-21 16:19:01 -0300

    A29 - Correção da instância de Adress

--- src/modulo02/aula29/view/MainView.java
+++ src/modulo02/aula29/view/MainView.java
@@ -2,7 +2,6 @@ package modulo02.aula29.view;
 
 import modulo02.aula29.controller.IndividualPersonController;
 import modulo02.aula29.controller.LegalPersonController;
-import modulo02.aula29.model.Adress;
 import modulo02.aula29.model.IndividualPerson;
 import modulo02.aula29.model.LegalPerson;
 
@@ -25,27 +24,23 @@ public class MainView {
         LegalPersonController legalController = new LegalPersonController();
         IndividualPerson individualPerson = new IndividualPerson();
         LegalPerson legalPerson = new LegalPerson();
-        Adress adress = new Adress();
 
         /** Atribuição de valores */
         individualPerson.name = "Laura Palmer";
         individualPerson.cpf = "987.225.467.12";
-        adress.adress = "Rua Bolinho";
-        adress.number = "17";
-        adress.zipCode = "98076-229";
-        adress.city = "Twin Peaks";
-        adress.state = "Washington";
-        individualPerson.homeAdress = adress;
+        individualPerson.homeAdress.adress = "Rua Bolinho";
+        individualPerson.homeAdress.number = "17";
+        individualPerson.homeAdress.zipCode = "98076-229";
+        individualPerson.homeAdress.city = "Twin Peaks";
+        individualPerson.homeAdress.state = "Washington";
 
-        adress = new Adress();
         legalPerson.name = "Dale Cooper";
         legalPerson.cnpj = "23.098.387/0001-23";
-        adress.adress = "Rua Richard";
-        adress.number = "34";
-        adress.zipCode = "98263-098";
-        adress.city = "Twin Peaks";
-        adress.state = "Washington";
-        legalPerson.businessAdress = adress;
+        legalPerson.businessAdress.adress = "Rua Richard";
+        legalPerson.businessAdress.number = "34";
+        legalPerson.businessAdress.zipCode = "98263-098";
+        legalPerson.businessAdress.city = "Twin Peaks";
+        legalPerson.businessAdress.state = "Washington";
 
         /** Adição no ArrayList */
         individualController.create(individualPerson);
@@ -62,13 +57,10 @@ public class MainView {
         }
 
         /** Update dos dois objetos alterando o nome da rua */
-        adress.adress = "Rua Bernice";
-        individualPerson.homeAdress.adress = adress.adress;
+        individualPerson.homeAdress.adress = "Rua Bernice";
         individualController.update(individualPerson);
 
-        adress = new Adress();
-        adress.adress = "Rua Coop";
-        legalPerson.businessAdress.adress = adress.adress;
+        legalPerson.businessAdress.adress = "Rua Coop";
         legalController.update(legalPerson);
 
         /** Impressão dos objetos no ArrayList */
@@ -83,16 +75,14 @@ public class MainView {
 
         /** Criando uma nova IndividualPerson */
         IndividualPerson individualPersonTwo = new IndividualPerson();
-        adress = new Adress();
 
         individualPersonTwo.name = "Audrey Horne";
         individualPersonTwo.cpf = "826.009.374.00";
-        adress.adress = "Rua Prudence";
-        adress.number = "18";
-        adress.zipCode = "98273-025";
-        adress.city = "Twin Peaks";
-        adress.state = "Washington";
-        individualPersonTwo.homeAdress = adress;
+        individualPersonTwo.homeAdress.adress = "Rua Prudence";
+        individualPersonTwo.homeAdress.number = "18";
+        individualPersonTwo.homeAdress.zipCode = "98273-025";
+        individualPersonTwo.homeAdress.city = "Twin Peaks";
+        individualPersonTwo.homeAdress.state = "Washington";
 
         /** Adição no ArrayList */
         individualController.create(individualPersonTwo);
