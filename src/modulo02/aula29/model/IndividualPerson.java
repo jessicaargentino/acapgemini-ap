a7b0d70172396074332712f9e0a2be5a37df86a6
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-10-21 15:23:47 -0300

    A29 - Criação da classe IndividualPerson

---
+++ src/modulo02/aula29/model/IndividualPerson.java
@@ -0,0 +1,10 @@
+package modulo02.aula29.model;
+
+public class IndividualPerson extends People {
+    public String cpf;
+
+    @Override
+    public String toString() {
+        return super.toString() + "\nCPF: " + this.cpf;
+    }
+}
