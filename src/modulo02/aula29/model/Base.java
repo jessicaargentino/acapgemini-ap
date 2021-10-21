80680d54ef66bf799e968a2ac840b97ed6181741
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-10-21 15:55:12 -0300

    A29 - Correção do counter

--- src/modulo02/aula29/model/Base.java
+++ src/modulo02/aula29/model/Base.java
@@ -2,7 +2,7 @@ package modulo02.aula29.model;
 
 public abstract class Base {
     public int id;
-    public static int counter = 0;
+    public static int counter = 1;
 
     public Base() {
         this.id = counter++;
