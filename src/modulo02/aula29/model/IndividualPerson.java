ee3ee6bb44f97d01150f0f5989563446de4da133
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-10-21 16:15:46 -0300

    A29 - Instância de Adress no Construtor

--- src/modulo02/aula29/model/IndividualPerson.java
+++ src/modulo02/aula29/model/IndividualPerson.java
@@ -3,6 +3,10 @@ package modulo02.aula29.model;
 public class IndividualPerson extends People {
     public String cpf;
 
+    public IndividualPerson() {
+        this.homeAdress = new Adress();
+    }
+
     @Override
     public String toString() {
         return super.toString() + "\nCPF: " + this.cpf + this.homeAdress.toString();
--- src/modulo02/aula29/model/LegalPerson.java
+++ src/modulo02/aula29/model/LegalPerson.java
@@ -3,6 +3,10 @@ package modulo02.aula29.model;
 public class LegalPerson extends People {
     public String cnpj;
 
+    public LegalPerson() {
+        this.businessAdress = new Adress();
+    }
+
     @Override
     public String toString() {
         return super.toString() + "\nCNPJ: " + this.cnpj + this.businessAdress.toString();
