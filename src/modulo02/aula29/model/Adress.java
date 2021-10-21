d66b56261c8fe79d837bf198c50413875066d705
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-10-21 15:19:55 -0300

    A29 - Criação classe Adress

---
+++ src/modulo02/aula29/model/Adress.java
@@ -0,0 +1,9 @@
+package modulo02.aula29.model;
+
+public class Adress {
+    public String adress;
+    public String number;
+    public String zipCode;
+    public String city;
+    public String state;
+}
