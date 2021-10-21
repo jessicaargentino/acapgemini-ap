c350f67e8dc092df624aa12d5562ee380ec8113c
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-10-21 15:31:56 -0300

    A29 - Instância dos controllers e models

--- src/modulo02/aula29/view/MainView.java
+++ src/modulo02/aula29/view/MainView.java
@@ -1,7 +1,19 @@
 package modulo02.aula29.view;
 
+import modulo02.aula29.controller.IndividualPersonController;
+import modulo02.aula29.controller.LegalPersonController;
+import modulo02.aula29.model.IndividualPerson;
+import modulo02.aula29.model.LegalPerson;
+
 public class MainView {
     public static void main(String[] args) {
-
+        /**
+         * Instância dos controllers e dos objetos das classes IndividualPerson e
+         * LegalPerson
+         */
+        IndividualPersonController individualController = new IndividualPersonController();
+        LegalPersonController legalController = new LegalPersonController();
+        IndividualPerson individualPerson = new IndividualPerson();
+        LegalPerson legalPerson = new LegalPerson();
     }
 }
