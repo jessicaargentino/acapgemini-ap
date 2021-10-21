e09910056c70adfa4b38357aabdf182870e09517
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-10-21 15:16:46 -0300

    A29 - Sobreescrita equals e toString

--- src/modulo02/aula29/model/People.java
+++ src/modulo02/aula29/model/People.java
@@ -1,6 +1,6 @@
 package modulo02.aula29.model;
 
-public class People {
+public abstract class People {
     public int id;
     public static int counter = 0;
     public String name;
@@ -9,5 +9,22 @@ public class People {
 
     public People() {
         this.id = counter++;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (object instanceof People) {
+            People people = (People) object;
+            if (this.id == people.id) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "\nID: " + this.id + "\nNome: " + this.name + "\nEndereço residencial: " + this.homeAdress
+                + "\nEndereço comercial: " + this.businessAdress;
     }
 }
