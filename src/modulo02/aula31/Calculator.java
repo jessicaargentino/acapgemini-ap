b44c3bae4f180c4ebb4f362ddf678d26696c2f00
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-10-25 10:18:26 -0300

    A31 - Criação dos quatro métodos: soma, subtração, multiplicação e adição

--- src/modulo02/aula31/Calculator.java
+++ src/modulo02/aula31/Calculator.java
@@ -1,5 +1,47 @@
 package modulo02.aula31;
 
 public class Calculator {
+    /**
+     * Método que realiza a soma entre dois números inteiros
+     * 
+     * @param numberOne 1º número informado
+     * @param numberTwo 2º número informado
+     * @return resultado da soma dos dois números
+     */
+    public static int sum(int numberOne, int numberTwo) {
+        return (numberOne + numberTwo);
+    }
 
+    /**
+     * Método que realiza a subtração entre dois números inteiros
+     * 
+     * @param numberOne 1º número informado
+     * @param numberTwo 2º número informado
+     * @return resultado da subtração dos dois números
+     */
+    public static int subtraction(int numberOne, int numberTwo) {
+        return (numberOne - numberTwo);
+    }
+
+    /**
+     * Método que realiza a multiplicação entre dois números inteiros
+     * 
+     * @param numberOne 1º número informado
+     * @param numberTwo 2º número informado
+     * @return resultado da multiplicação dos dois números
+     */
+    public static int multiplication(int numberOne, int numberTwo) {
+        return (numberOne * numberTwo);
+    }
+
+    /**
+     * Método que realiza a divisão entre dois números inteiros
+     * 
+     * @param numberOne 1º número informado
+     * @param numberTwo 2º número informado
+     * @return resultado da divisão dos dois números
+     */
+    public static int division(int numberOne, int numberTwo) {
+        return (numberOne / numberTwo);
+    }
 }
