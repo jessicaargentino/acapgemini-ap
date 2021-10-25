e32b539c7fccb3ce6ead45630ee795feddc27b81
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-10-25 18:06:55 -0300

    A31 - Getters e setters

--- src/modulo02/aula31/a02/model/Calculator.java
+++ src/modulo02/aula31/a02/model/Calculator.java
@@ -1,5 +1,23 @@
 package modulo02.aula31.a02.model;
 
 public class Calculator {
-    
+    private int numberOne;
+    private int numberTwo;
+
+    /** Getters e Setters */
+    public void setNumberOne(int numberOne) {
+        this.numberOne = numberOne;
+    }
+
+    public int getNumberOne() {
+        return numberOne;
+    }
+
+    public void setNumberTwo(int numberTwo) {
+        this.numberTwo = numberTwo;
+    }
+
+    public int getNumberTwo() {
+        return numberTwo;
+    }
 }
