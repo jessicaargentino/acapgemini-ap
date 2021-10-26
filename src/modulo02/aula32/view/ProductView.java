b28cc38a62cf90b04c89d4e8e14c3dbb4ba08c09
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-10-26 10:25:02 -0300

    A32 - Criação do método menu

--- src/modulo02/aula32/view/ProductView.java
+++ src/modulo02/aula32/view/ProductView.java
@@ -1,7 +1,18 @@
 package modulo02.aula32.view;
 
+import modulo02.aula32.controller.ProductController;
+
 public class ProductView {
     public static void main(String[] args) {
+        System.out.println("****** CADASTRO DE PRODUTOS ******\n");
+        menu();
+    }
 
+    /** Método que imprime um menu com as opções do programa */
+    public static void menu() {
+        System.out.println("1 - Adicionar um produto");
+        System.out.println("2 - Alterar um produto");
+        System.out.println("3 - Deletar um produto");
+        System.out.println("4 - Listar produtos");
     }
 }
