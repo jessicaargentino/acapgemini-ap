d8331efcd3c0b0041780a9f7845a9afb379e7def
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-10-26 10:53:20 -0300

    A32 - Método indexOf

--- src/modulo02/aula32/controller/ProductController.java
+++ src/modulo02/aula32/controller/ProductController.java
@@ -1,6 +1,26 @@
 package modulo02.aula32.controller;
 
+import java.util.ArrayList;
+
 import modulo02.aula32.model.Product;
 
 public class ProductController extends BaseController<Product> {
+    /**
+     * Método que percorre o ArrayList afim de verificar se existe objeto com o id
+     * informado. Se sim, retorna o index/posição onde o objeto foi encontrado, se
+     * não, retorna -1.
+     * 
+     * @param id id informado pelo usuário
+     * @return >= 0 para encontrado, -1 para não encontrado
+     */
+    public int indexOf(int id) {
+        ArrayList<Product> product = this.read();
+        for (int i = 0; i < product.size(); i++) {
+            Product p = product.get(i);
+            if (p.getId() == id) {
+                return i;
+            }
+        }
+        return -1;
+    }
 }
