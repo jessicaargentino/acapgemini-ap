4b9d8d8f801c468d42a18f10e1ff5ea585cfa268
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-10-26 09:52:29 -0300

    A32 - Definição dos atributos e construtor

--- src/modulo02/aula32/model/Product.java
+++ src/modulo02/aula32/model/Product.java
@@ -1,5 +1,20 @@
 package modulo02.aula32.model;
 
-public class Product {
+import modulo02.aula32.model.Category;
+
+public abstract class Product extends Base {
+    private String name;
+    private String brand;
+    private Category category;
+    private int quantity;
+    private double price;
+
+    /**
+     * Construtor - Cria uma instância da classe Category a cada instância de
+     * Product
+     */
+    public Product() {
+        category = new Category();
+    }
 
 }
