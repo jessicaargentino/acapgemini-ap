eb052aa97effbc5b882732f4d1b9b5e3c256d049
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-10-26 09:45:59 -0300

    A32 - Definição do atributo id, counter e declaração do getter

--- src/modulo02/aula32/model/Base.java
+++ src/modulo02/aula32/model/Base.java
@@ -1,5 +1,18 @@
 package modulo02.aula32.model;
 
-public class Base {
-    
+public abstract class Base {
+    private int id;
+    private static int counter = 0;
+
+    /**
+     * Construtor - Através do counter, incrementa o id a cada instância da classe
+     */
+    public Base() {
+        this.id = counter++;
+    }
+
+    /** Getter */
+    public int getId() {
+        return id;
+    }
 }
