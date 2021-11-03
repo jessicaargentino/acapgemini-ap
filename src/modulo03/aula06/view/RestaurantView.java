60591bb0a9e1146011db98a03d50eaa7366d0025
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-11-03 11:13:37 -0300

    A06 - Atribuiçãod de valores

--- src/modulo03/aula06/view/RestaurantView.java
+++ src/modulo03/aula06/view/RestaurantView.java
@@ -1,7 +1,19 @@
 package modulo03.aula06.view;
 
+import modulo03.aula06.controller.RestaurantController;
+import modulo03.aula06.model.Restaurant;
+
 public class RestaurantView {
-   public static void main(String[] args) {
-       
-   } 
+    public static void main(String[] args) {
+        /** Criação de um objeto da classe RestaurantController e Restaurant */
+        RestaurantController restaurantController = new RestaurantController();
+        Restaurant restaurant = new Restaurant();
+
+        /** Atribuição de valores e adição no ArrayList */
+        restaurant.setName("Nhoque ao gorgonzola");
+        restaurant.setDescription("Massa de batata inglesa com queijo gorgonzola");
+        restaurant.setPrice(29.90);
+        restaurantController.create(restaurant);
+
+    }
 }
