1df1931050f3d32c569b205cdecce27dd5b5fe85
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-11-04 20:06:59 -0300

    A07 - Inclusão do enunciado

--- src/modulo03/aula07/atv02/view/TouristViewOne.java
+++ src/modulo03/aula07/atv02/view/TouristViewOne.java
@@ -3,6 +3,15 @@ package modulo03.aula07.atv02.view;
 import modulo03.aula07.atv02.controller.TouristAttractionController;
 import modulo03.aula07.atv02.model.TouristAttraction;
 
+/**
+ * Crie um sistema para cadastro de Pontos Turísticos. A classe de modelo deve
+ * possuir três atributos públicos: nome, descrição e localização. Crie uma
+ * interface que contenha os métodos de leitura e gravação de dados para pontos
+ * turísticos. Crie duas implementações da interface. Uma que usará um lista
+ * para armazenar os dados e outra que utilizará um arquivo texto como fonte de
+ * dados. Crie duas classes View para testar cada uma das implementações
+ * criadas.
+ */
 public class TouristViewOne {
     public static void main(String[] args) {
         /**
