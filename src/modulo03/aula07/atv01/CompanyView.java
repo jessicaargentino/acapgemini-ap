551072461dba94e650e051ca2af12feefcecf954
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-11-04 18:13:47 -0300

    A07 - Inclusão do enunciado

--- src/modulo03/aula07/atv01/CompanyView.java
+++ src/modulo03/aula07/atv01/CompanyView.java
@@ -1,5 +1,14 @@
 package modulo03.aula07.atv01;
 
+/**
+ * Crie um sistema para cadastro de Empresas. A empresa deve possuir três
+ * atributos públicos: nome, cnpj e ramo de atividade. Utilize o padrão DAO para
+ * criar uma estrutura de armazenamento de dados em um arquivo do tipo CSV. A
+ * estrutura de armazenamento deve apenas salvar e ler os dados. Os dados devem
+ * ser convertidos de Objeto para String ao salvar. Os dados devem ser
+ * convertidos de String para Objeto ao ler. Utilize os métodos de leitura e
+ * escrita em uma classe View.
+ */
 public class CompanyView {
     public static void main(String[] args) {
         /** Criação de um objeto da classe Company e atribuição de valores */
