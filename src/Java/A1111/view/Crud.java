a9c0f8be79e2f0668c2ccecbb2e156619f5537e5
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-11-11 18:39:40 -0300

    ATP46 - Inclusão do enunciado

--- src/Java/A1111/view/Crud.java
+++ src/Java/A1111/view/Crud.java
@@ -5,6 +5,16 @@ import Java.A1111.model.Delete;
 import Java.A1111.model.Read;
 import Java.A1111.model.Update;
 
+/**
+ * Utilize as tabelas e os registros criados durante as atividades ATP41 e
+ * ATP42. Faça o download do driver JDBC para o postgres e configure no seu
+ * projeto. Crie uma conexão com o banco de dados postgresql utilizando
+ * JDBC(java.sql.*). Crie 4 classes para realizar as 4 operações de CRUD. Na
+ * classe1, liste todos os dados de todas as colunas da tabela produto. Na
+ * classe2, insira um novo produto na tabela e imprima o id gerado. Na classe3,
+ * altere todos os produtos para a mesma categoria e exiba a quantidade de
+ * linhas afetadas. Na classe4, delete um produto pelo id.
+ */
 public class Crud {
     public static void main(String[] args) {
         /** Criação de objetos das classes */
