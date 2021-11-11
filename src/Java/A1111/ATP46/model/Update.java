47b6e34b103b96f7cb19f66c1e2c23a222f54f1f
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-11-11 19:52:56 -0300

    ATP46 - Classe com método update utilizando bd

---
+++ src/Java/A1111/ATP46/model/Update.java
@@ -0,0 +1,38 @@
+package Java.A1111.ATP46.model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Update {
+    /**
+     * Método que cria uma conexão com banco de dados Postgres e realiza a
+     * alteração/update na tabela produto, alterando o ID de todos os produtos para
+     * 3
+     */
+    public void update() {
+        try {
+            String driverType = "jdbc";
+            String driverName = "postgresql";
+            String host = "localhost";
+            int port = 5432;
+            String database = "postgres";
+            String user = "postgres";
+            String password = "admin";
+
+            String connectionString = String.format("%s:%s://%s:%d/%s", driverType, driverName, host, port, database);
+            Connection connection = DriverManager.getConnection(connectionString, user, password);
+
+            String query = "UPDATE produto SET id_categoria = 3";
+            Statement statement = connection.createStatement();
+            statement.execute(query);
+            int linesAffected = statement.getUpdateCount();
+            System.out.println(linesAffected);
+
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
