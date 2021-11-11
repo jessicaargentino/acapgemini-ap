f478da43dad8d7fe0ca449ac9a59cbf02cdd1277
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-11-11 10:00:16 -0300

    ATP43 - Exercício de banco de dados, DQL

--- src/bd/ATP43 - DQL.sql
+++ src/bd/ATP43 - DQL.sql
@@ -9,63 +9,24 @@
 -- Liste os dados de todas as colunas da tabela Categoria e o os dados da coluna nome da tabela Produto, para as categorias que estão sendo utilizadas por produtos.
 
 -- DADOS DA TABELA CATEGORIA
-select
-	*
-from
-	categoria;
+select * from categoria;
 
 -- DADOS DA TABELA PRODUTO
-select
-	*
-from
-	produto;
+select * from produto;
 
 -- DADOS DA TABELA CATEGORA QUE POSSUAM DESCRIÇÃO
-select
-	*
-from
-	categoria
-where
-	descricao <> '';
+select * from categoria where descricao <> '';
 
 -- DADOS DA TABELA PRODUTO QUE COMECEM COM C
-select
-	id,
-	nome,
-	preco
-from
-	produto
-where
-	nome like 'C%';
+select id, nome, preco from produto where nome like 'C%';
 
 -- DADOS DAS TABELAS PRODUTO E CATEGORIA COM PREÇO > 0
-select
-	p.id,
-	p.nome,
-	p.preco,
-	c.id,
-	c.nome,
-	c.descricao
-from
-	produto as p
-join categoria as c on
-	p.id_categoria = c.id
-where
-	p.preco > 0;
+select p.id, p.nome, p.preco, c.id, c.nome, c.descricao 
+from produto as p join categoria as c on p.id_categoria = c.id where p.preco > 0;
 	
 -- DADOS DA TABELA PRODUTO COM ID_CATEGORIA > 2
-select
-	*
-from
-	produto
-where
-	id_categoria > 2;
+select * from produto where id_categoria > 2;
 	
 -- DADOS DA TABELA CATEGORIA E NOME DA TABELA PRODUTO PARA PRODUTOS QUE POSSUEM CATEGORIA
-select
-	c.*,
-	p.nome
-from
-	categoria as c
-join produto as p on
-	c.id = p.id_categoria;
+select c.*, p.nome from categoria as c
+join produto as p on c.id = p.id_categoria;
