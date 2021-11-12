d032166d5ecf3f9ea156cdd0bd6378cfc01dcfe4
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-11-12 12:00:46 -0300

    ATP48 - Classe com método read utilizando PreparedStatement

---
+++ src/Java/A1211/ATP48/model/Read.java
@@ -0,0 +1,46 @@
+package Java.A1211.ATP48.model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Read {
+    /**
+     * Método que cria uma conexão com banco de dados Postgres e realiza a seleção
+     * do id e nome de todos os registros da tabela categoria.
+     */
+    public void read() {
+        try {
+            String driverType = "jdbc";
+            String driverName = "postgresql";
+            String host = "localhost";
+            int port = 5432;
+            String database = "postgres";
+            String user = "postgres";
+            String password = "admin";
+
+            String stringConnection = String.format("%s:%s://%s:%d/%s", driverType, driverName, host, port, database);
+            Connection connection = DriverManager.getConnection(stringConnection, user, password);
+
+            String query = "SELECT id, nome FROM categoria";
+            PreparedStatement preparedStatement = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+            preparedStatement.execute();
+            ResultSet result = preparedStatement.getResultSet();
+
+            while (result.next()) {
+                int id = result.getInt("id");
+                String nome = result.getString("nome");
+
+                System.out.printf("ID: %d\nNome: %s\n\n", id, nome);
+            }
+
+            connection.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
