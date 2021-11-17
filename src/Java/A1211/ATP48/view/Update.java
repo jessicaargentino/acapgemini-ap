dea30037f3e87c4ee5f740e5f527b98707d51c22
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-11-17 10:02:24 -0300

    ATP48/12.11 - Classe com método Main e realização de update utilizando PreparedStatement

--- src/Java/A1211/ATP48/view/Update.java
+++ src/Java/A1211/ATP48/view/Update.java
@@ -1,4 +1,4 @@
-package Java.A1211.ATP48.model;
+package Java.A1211.ATP48.view;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -7,12 +7,13 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class Update {
-    /**
-     * Método que cria uma conexão com banco de dados Postgres e realiza a
-     * alteração/update na tabela categoria, alterando o nome das categorias de id 1
-     * e 3 para móveis.
-     */
-    public void update() {
+    public static void main(String[] args) {
+        /**
+         * Criação de uma conexão com banco de dados Postgres e realiza a
+         * alteração/update na tabela categoria, alterando o nome das categorias de id 1
+         * e 3 para móveis, desta vez utilizando PreparedStatement para prevenir SQL
+         * Injection.
+         */
         try {
             String driverType = "jdbc";
             String driverName = "postgresql";
@@ -37,7 +38,6 @@ public class Update {
 
             int linesAffecteds = preparedStatement.getUpdateCount();
             System.out.println(linesAffecteds);
-
             connection.close();
         } catch (SQLException e) {
             e.printStackTrace();
