82f823b1107c18868963cf5323bb15e8adcbef1d
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-11-17 10:29:19 -0300

    ATP49/12.11 - Classe que realiza e retorna conexão com banco de dados

--- src/Java/A1211/ATP49/utils/ConnectionFactory.java
+++ src/Java/A1211/ATP49/utils/ConnectionFactory.java
@@ -1,35 +1,39 @@
 package Java.A1211.ATP49.utils;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 
-import javax.sql.DataSource;
-import com.mchange.v2.c3p0.ComboPooledDataSource;
-
 public class ConnectionFactory {
-    private DataSource dataSource;
+    private String driverType;
+    private String driverName;
+    private String host;
+    private int port;
+    private String database;
+    private String user;
+    private String password;
 
-    /**
-     * Construtor que através do mchange/ComboPooledDataSource cria um pool com 10
-     * conexões no nosso banco
-     */
+    /** Construtor que inicia os atributos da classe */
     public ConnectionFactory() {
-        ComboPooledDataSource pool = new ComboPooledDataSource();
-        pool.setJdbcUrl("jdbc:postgresql://192.168.0.18:5432/postgres");
-        pool.setUser("postgres");
-        pool.setPassword("admin");
-        pool.setMaxPoolSize(10);
-        this.dataSource = pool;
+        driverType = "jdbc";
+        driverName = "postgresql";
+        host = "localhost";
+        port = 5432;
+        database = "postgres";
+        user = "postgres";
+        password = "admin";
     }
 
     /**
-     * Método do tipo Connection que retorna o datasource (interface do java que
-     * abstrai a conexão)
+     * Método que através de uma String formatada com o pattern da conexão, retorna
+     * a conexão.
      * 
-     * @return o pool de conexões (retorna uma das conexões do mesmo)
+     * @return conexão com o banco de dados
      * @throws SQLException
      */
     public Connection getConnection() throws SQLException {
-        return this.dataSource.getConnection();
+        String stringConnection = String.format("%s:%s://%s:%d/%s", this.driverType, this.driverName, this.host,
+                this.port, this.database);
+        return DriverManager.getConnection(stringConnection, user, password);
     }
 }
