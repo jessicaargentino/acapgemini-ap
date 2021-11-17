fd4fca4b053f556287db71ee994ba92bf2546d40
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-11-17 09:43:47 -0300

    ATP46/11.11 - Classe com método Main e realização de insert utilizando BD

--- src/Java/A1111/ATP46/view/Create.java
+++ src/Java/A1111/ATP46/view/Create.java
@@ -1,4 +1,4 @@
-package Java.A1111.ATP46.model;
+package Java.A1111.ATP46.view;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -6,12 +6,23 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+/**
+ * Utilize as tabelas e os registros criados durante as atividades ATP41 e
+ * ATP42. Faça o download do driver JDBC para o postgres e configure no seu
+ * projeto. Crie uma conexão com o banco de dados postgresql utilizando
+ * JDBC(java.sql.*). Crie 4 classes para realizar as 4 operações de CRUD. Na
+ * classe1, liste todos os dados de todas as colunas da tabela produto. Na
+ * classe2, insira um novo produto na tabela e imprima o id gerado. Na classe3,
+ * altere todos os produtos para a mesma categoria e exiba a quantidade de
+ * linhas afetadas. Na classe4, delete um produto pelo id.
+ */
+
 public class Create {
-    /**
-     * Método que cria uma conexão com banco de dados Postgres e realiza a inserção
-     * de um novo registro na tabela produto.
-     */
-    public void create() {
+    public static void main(String[] args) {
+        /**
+         * Criação de uma conexão com banco de dados Postgres e realiza a inserção de um
+         * novo registro na tabela produto.
+         */
         try {
             String driverType = "jdbc";
             String driverName = "postgresql";
