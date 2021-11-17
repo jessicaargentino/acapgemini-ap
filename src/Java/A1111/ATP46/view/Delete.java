61a3e65ae3d152357ae68aeb21125f6e8a808d4e
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-11-17 09:45:22 -0300

    ATP46/11.11 - Classe com método Main e realização de delete utilizando BD

--- src/Java/A1111/ATP46/view/Delete.java
+++ src/Java/A1111/ATP46/view/Delete.java
@@ -1,17 +1,16 @@
-package Java.A1111.ATP46.model;
+package Java.A1111.ATP46.view;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class Update {
-    /**
-     * Método que cria uma conexão com banco de dados Postgres e realiza a
-     * alteração/update na tabela produto, alterando o ID de todos os produtos para
-     * 3
-     */
-    public void update() {
+public class Delete {
+    public static void main(String[] args) {
+        /**
+         * Criação de uma conexão com banco de dados Postgres e realiza a exclusão do
+         * registro de id 2 na tabela produto.
+         */
         try {
             String driverType = "jdbc";
             String driverName = "postgresql";
@@ -23,8 +22,7 @@ public class Update {
 
             String connectionString = String.format("%s:%s://%s:%d/%s", driverType, driverName, host, port, database);
             Connection connection = DriverManager.getConnection(connectionString, user, password);
-
-            String query = "UPDATE produto SET id_categoria = 3";
+            String query = "DELETE FROM produto WHERE id = 2";
             Statement statement = connection.createStatement();
             statement.execute(query);
             int linesAffected = statement.getUpdateCount();
